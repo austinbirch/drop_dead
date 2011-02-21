b990ef4457d8b5f49906eff21fffc7a9705ce963
Austin Birch <mraustinbirch@gmail.com>
2011-02-21 17:37:01 +0000

    Added lives left images.'

--- js/Game.js
+++ js/Game.js
@@ -81,6 +81,8 @@ var Game = function(){
 	
 	//player sprite sheet
 	this.imageManager.addImage("runnerSpriteSheet", "./images/runnerPlayer.png");
+	//player lives
+	this.imageManager.addImage("heart", "./images/heart.png");
 	
 	//background
 	this.imageManager.addImage("bg", "./images/bg.png");
@@ -499,6 +501,7 @@ Game.prototype.update = function(delta){
 						//check for player death					
 						if (this.player.lives <= 0){
 							//player is dead!
+							this.player.lives = 0;
 							this.alert_message = "YOU'RE DEAD!";
 							this.status = GAME_OVER;
 							if (this.alert_opacity != 0.7){
@@ -671,7 +674,7 @@ Game.prototype.draw = function(){
 	// }
 	
 	//display the level
-	this.context.fillText("level: " + this.level + "/" + this.max_level, (this.canvas.width() - 100), 35);
+	this.context.fillText("level: " + this.level + "/" + this.max_level, (this.canvas.width() - 100), 40);
 	
 	//draw the target line
 	this.context.fillStyle = "rgba(255, 255, 255, 0.5)";
--- js/RunnerPlayer.js
+++ js/RunnerPlayer.js
@@ -36,6 +36,7 @@ var RunnerPlayer = function(){
 	
 	this.max_lives = 2;
 	this.lives = this.max_lives;
+	this.lives_image = null; 
 	
 	//default acceleration
 	this.defaultAcceleration = 5;
@@ -92,6 +93,9 @@ RunnerPlayer.prototype.setPlayerImage = function(imageManager) {
 		{ sprite: 'stand_rev' }
 	], player_sprite_sheet);
 	
+	//set the lives image
+	this.lives_image = imageManager.getImage("heart");
+	
 };
 
 RunnerPlayer.prototype.update = function(delta) {
@@ -127,7 +131,15 @@ RunnerPlayer.prototype.draw = function(ctx) {
 	ctx.fillText(this.player_name, (this.position.x - 10), (this.position.y - 5));
 
 	//draw the player lives 
-	ctx.fillText("lives left: " + this.lives, (ctx.canvas.width - 100), 20);
+	ctx.fillText("lives left: ", (ctx.canvas.width - 136), 20);
+	for (var i = this.lives; i > 0; i--){
+		ctx.drawImage(this.lives_image, (ctx.canvas.width - 69) + (i * 16), 10);
+	};
+	
+	if (this.lives == 0){
+		ctx.fillText("none!", (ctx.canvas.width - 55), 20);
+	}
+	
 };
 
 //set the position of the player
